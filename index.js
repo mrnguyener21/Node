3dd48d1d2d64bcb4a18f2a7d8885a2241161be36
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-07-09 07:22:20 -0700

    learned about delete andd patch in order to delete or update objects within the back end

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const express = require('express');
 const { response } = require('express');
+const bodyParser = require('body-parser');
+const { v4: uuid } = require('uuid');
 const app = express()
 const port = 5000;
-const bodyParser = require('body-parser');
-
-// Use UUID which we talked about while doing redux.
-// Add a unique ID to each created user before it's added to the "database"
-
-// Make a delete route to /people/:id
-// and remove that user from the "database" - hint db is an array, so just remove it normally.
 
 app.use(bodyParser.json());
 
-const users = [];
+let users = [];
 
 app.get('/', (request, response) => {
     console.log('Reached Route /');
     
-    response.send('Hello Bobby!');
+    response.send('Social Media App');
 });
 
 app.post('/person', (req, res) => {   
     const user = req.body;
 
-    users.push(user);
+    users.push({...user, id: uuid()});
     
     console.log(`User [${user.username}] added to the database.`);
 });
@@ -34,8 +29,62 @@ app.get('/people', (req, res) => {
     res.send(users);
 });
 
-app.delete('/people', (req, res) => {
+
+app.get('/people/:id', (req, res) => {
+    res.send(req.params.id)
+});
+
+app.delete('/people/:id', (req, res) => {
+    console.log(`user with id ${req.params.id} has been deleted`);
     
+    users = users.filter((user) => user.id !== req.params.id);
+});
+
+app.post('*', (req, res) => {
+    console.log('YOU TRIED DOING SOMETHING WITH A ROUTE THAT DOESNT EXIST');
 })
 
 app.listen(port, () => console.log(`Server Running on Port: http://localhost:${port}`));
+
+app.patch('/people/:id', (req,res) => {
+    // users = users.map((user) => {
+    //     if (user.id === req.params.id) {
+    //         user.age += 1;
+    //     }
+
+    //     return user;
+    // });
+
+    const user = users.find((user) => user.id === req.params.id);
+
+    user.username = 'something'
+    user.age = 25;
+})
+
+// 1	GET
+// The GET method is used to retrieve information from the given server using a given URI. Requests using GET should only retrieve data and should have no other effect on the data.
+
+// 2	HEAD
+// Same as GET, but it transfers the status line and the header section only.
+
+// 3	POST
+// A POST request is used to send data to the server, for example, customer information, file upload, etc. using HTML forms.
+
+// 4	PUT
+// Replaces all the current representations of the target resource with the uploaded content.
+
+// { USER } => { USER... }
+
+// 5	DELETE
+// Removes all the current representations of the target resource given by URI.
+
+// 6	CONNECT
+// Establishes a tunnel to the server identified by a given URI.
+
+// 7	OPTIONS
+// Describe the communication options for the target resource.
+
+// 8	TRACE
+// Performs a message loop back test along with the path to the target resource.
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PATCH
