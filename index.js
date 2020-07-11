12f9bcd4a5a25d423e798cd02ff068a3c879ff0c
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-07-11 07:44:03 -0700

    learned how to separate the logic for node/express app and small intro into middleware

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
-const express = require('express');
-const { response } = require('express');
-const bodyParser = require('body-parser');
-const { v4: uuid } = require('uuid');
-const app = express()
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js'
+
+//index.js is for the setup while separtae files are for specific logic path
+
+const app = express();
 const port = 5000;
+
+var myLogger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+}
+
+app.use(myLogger)
 
 app.use(bodyParser.json());
 
-let users = [];
-
-app.get('/', (request, response) => {
+app.use('/people', usersRoutes);
+ 
+app.get('/', (request, response) => {  
     console.log('Reached Route /');
     
     response.send('Social Media App');
-});
-
-app.post('/person', (req, res) => {   
-    const user = req.body;
-
-    users.push({...user, id: uuid()});
-    
-    console.log(`User [${user.username}] added to the database.`);
-});
-
-app.get('/people', (req, res) => {
-    console.log(`Users in the database: ${users}`);
-
-    res.send(users);
-});
-
-
-app.get('/people/:id', (req, res) => {
-    res.send(req.params.id)
-});
-
-app.delete('/people/:id', (req, res) => {
-    console.log(`user with id ${req.params.id} has been deleted`);
-    
-    users = users.filter((user) => user.id !== req.params.id);
 });
 
 app.post('*', (req, res) => {
@@ -46,43 +31,4 @@ app.post('*', (req, res) => {
 
 app.listen(port, () => console.log(`Server Running on Port: http://localhost:${port}`));
 
-// change the username and the age not statically, but rather based on the body sent from the frontend (postman);
 
-app.patch('/people/:id', (req,res) => {
-    const user = users.find((user) => user.id === req.params.id);
-    
-    user.username = req.body.username;
-    user.age = req.body.age;
-
-    console.log(`username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`)
-    // user.username = 'something'
-    // user.age = 25;
-})
-
-// 1	GET
-// The GET method is used to retrieve information from the given server using a given URI. Requests using GET should only retrieve data and should have no other effect on the data.
-
-// 2	HEAD
-// Same as GET, but it transfers the status line and the header section only.
-
-// 3	POST
-// A POST request is used to send data to the server, for example, customer information, file upload, etc. using HTML forms.
-
-// 4	PUT
-// Replaces all the current representations of the target resource with the uploaded content.
-
-// { USER } => { USER... }
-
-// 5	DELETE
-// Removes all the current representations of the target resource given by URI.
-
-// 6	CONNECT
-// Establishes a tunnel to the server identified by a given URI.
-
-// 7	OPTIONS
-// Describe the communication options for the target resource.
-
-// 8	TRACE
-// Performs a message loop back test along with the path to the target resource.
-
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PATCH
---
+++ routes/users.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { getUsers, createUser, getUser, deleteUser, updateUser } from '../controllers/users.js';
+
+const router = express.Router();
+
+// READ/GET all users
+router.get('/', getUsers);
+
+// CREATE/POST a new user
+router.post('/', createUser);
+
+// READ/GET an individual user
+router.get('/:id', getUser)
+
+// DELETE/DELETE an individual user
+router.delete('/:id', deleteUser);
+
+// UPDATE/PATCH an individual user
+router.patch('/:id', updateUser);
+
+export default router;
